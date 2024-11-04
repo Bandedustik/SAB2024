c0f18d96ed846b7b06a22029893d091e46b5b092
Антон <romanenkoquery@yandex.ru>
2024-11-04 10:52:20 +0300

    Поехали!

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 # Коммент свыше
 
+# Третий коммент
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
